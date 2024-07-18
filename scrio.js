246c31ce1e181dd381c48c692c27dfb10e1311ef
123Kevin4567 <Kevinpolodr@gmail.com>
2024-07-18 11:36:06 -0500

    jueves 18

---
+++ scrio.js
@@ -0,0 +1,5 @@
+const menuOpciones = document.querySelector(".menu-opciones");
+const btnSignup = document.getElementById("btn-sign-up");
+const header =document.querySelector("header");
+const controlesUsuarios = document
+
